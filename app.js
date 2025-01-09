4418fcdfeeb856055ad8b852285c746059a810fe
jmaultsby <ironmanmadeit@outlook.com>
2025-01-08 21:55:35 -0500

    Homework Complete

--- app.js
+++ app.js
@@ -27,15 +27,106 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-const isAdult = (adult) => {
-    if (21<) {
-        return adult;
+const isAdult = (age) => {
+    if (age>=18){
+        return "adult";
     } else {
-        return minor;
+        return "minor";
     }
 }
 
 
 console.log('Exercise 2 Result:', isAdult(21));
 
-  
+  /*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+
+
+
+const isCharAVowel = (x) => {
+  if (x=='a') {
+    return true;
+  }
+  else if (x=='i'){
+      return true;
+  }
+   else if (x=='e'){
+      return true;
+  }
+   else if (x=='o'){
+      return true;
+  }
+   else if (x=='u'){
+      return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("o"));
+
+const generateEmail = (x,y) => {
+  return x + '@' + y;
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+const greetUser = (x,y) => {
+  return "Good " + y + ', ' + x +"!";
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+const maxOfThree = (x,y, z) => {
+   if (x >= y && x >=z) {
+    return x;
+  } else if(y >= x && y >=z) {
+    return y;
+  } else{
+      return z;
+  }
+}
+console.log('Exercise 6 Result:', maxOfThree(5, 4, 8));
+
+const calculateTip = (x,y) => {
+  return x * (y/100);
+}
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+const convertTemperature = (x,y) => {
+  if (y =="C") {
+    return x * 9 / 5 + 32;
+  } else {
+    return (x - 32) * 5 / 9;
+
+  }
+}
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+const basicCalculator = (x,y,z) => {
+  if (z =="subtract") {
+    return x-y;
+  } else if (z== "add") {
+    return x + y;
+  } else if (z== "multiply") {
+    return x * y;
+  } else if (z== "divide") {
+    return x /y;
+  }
+}
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
+
+
+
